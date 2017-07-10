dac0a529b460fc79cb77a5446a70618349d66357
danny01.feng <danny01.feng@danny01-feng.corp.vipshop.com>
2017-07-10 18:21:44 +0800

    乖巧小王子写了一个定时器来玩玩

--- src/main/java/com/turtlebone/core/Application.java
+++ src/main/java/com/turtlebone/core/Application.java
@@ -2,10 +2,14 @@ package com.turtlebone.core;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.turtlebone.core")
 @MapperScan("com.turtlebone.core.repository") 
+@EnableScheduling
+@EnableAutoConfiguration
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
